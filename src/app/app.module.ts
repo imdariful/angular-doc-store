d0c3eb63554d0c9dbdf652673220d1765e5c96dd
Ariful Islam <ariful.rony10@gmail.com>
2023-10-13 22:47:01 +0600

    fetch data from api and display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
+
     RouterModule.forRoot([
       {
         path: '',
@@ -27,6 +31,10 @@ import { CartComponent } from './cart/cart.component';
         path: 'cart',
         component: CartComponent,
       },
+      {
+        path: 'shipping',
+        component: ShippingComponent,
+      },
     ]),
   ],
   declarations: [
@@ -36,6 +44,7 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
+    ShippingComponent,
   ],
   bootstrap: [AppComponent],
 })
